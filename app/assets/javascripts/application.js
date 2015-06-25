a6b97e26f28f6377201196cc56878debd3906313
Lucas Santana Lellis <lucaslellis777@gmail.com>
2015-06-25 18:22:56 +0000

    Separei o layout em módulos.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,51 @@
 //= require bootstrap-sprockets
 //= require bootstrap-material-design
 //= require_tree .
+
+var setTooltip = function (elm, placement, text) {
+    elm.attr('rel','tooltip');
+    elm.attr('data-placement',placement);
+    elm.attr('data-toggle','tooltip');
+    elm.attr('title','');
+    elm.attr('data-original-title',text);
+    elm.tooltip();
+}
+
+var sendMessage = function(message) {
+    $('#alert').text(message);
+    $('#alert').fadeIn();
+    $("#alert").delay(2000).fadeOut("slow", function () { $(this).hide(); });
+}
+
+var inicializar = function() {
+    $.material.init();
+    //   $('#modal').modal('show')
+    var links = [
+        {
+            "bgcolor":"#FFC107",
+            "icon":"+",
+            "id" : "postar"
+        },
+        {
+            // "url":"#",
+            "bgcolor":"#3F51B5",
+            "color":"#fffff",
+            "icon":"<i class='mdi-image-photo'></i>",
+            "target":"_blank",
+            "id" : "postar-foto"
+        },
+        {
+        // "url":"#",
+            "bgcolor":"#FF5252",
+            "color":"white",
+            "icon":"<i class='mdi-image-edit'></i>", 
+            "id" : "postar-texto"
+        }
+    ]
+    $('.kc_fab_wrapper').kc_fab(links);
+    $('#postar-texto').attr('onclick',"#modal");
+    $('#postar-foto').attr('onclick',"#modal-foto");
+    setTooltip( $('#postar-texto'), "left", "Texto" );
+    setTooltip( $('#postar-foto'), "left", "Imagem" );
+    setTooltip( $('#postar'), "left", "Postar" );
+}
